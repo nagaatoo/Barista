425d3753aa30b8c6bccbdc8e919eead011e04ef1
e.khodosov <e.khodosov@itfbgroup.ru>
2022-06-25 17:34:31 +0300

    Fixed mount conf into props and script pt 2

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 docker container stop barista
 docker rm barista
 docker build --tag=barista:latest .
-docker run -p 8551:8551 -v /home/nagatoo/baristaData:/mnt/baristaData --add-host=database:172.17.0.1 --name barista barista:latest
+nohup docker run -p 8551:8551 -v /home/nagatoo/baristaData:/mnt/baristaData --add-host=database:172.17.0.1 --name barista barista:latest &>/dev/null &
