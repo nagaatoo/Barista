2eb8e7a670d0c7576f27c420b4746eb0c6765381
e.khodosov <e.khodosov@itfbgroup.ru>
2022-06-25 17:30:00 +0300

    Fixed mount conf into props and script

--- run.cmd
+++ run.cmd
@@ -1,3 +1,5 @@
 call ./gradlew build -x test
+call docker container stop barista
+call docker rm barista
 call docker build --tag=barista:latest .
 call docker run -p 8551:8551 -v /home/nagatoo/baristaData:/mnt/baristaData --add-host=database:172.17.0.1 --name barista barista:latest
