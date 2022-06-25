431df8886ec9eed81531469a43db5ed001a4f4c7
e.khodosov <e.khodosov@itfbgroup.ru>
2022-06-25 17:24:16 +0300

    Added mount conf into props and script

--- run.cmd
+++ run.cmd
@@ -1,3 +1,3 @@
 call ./gradlew build -x test
 call docker build --tag=barista:latest .
-call docker run -p 8551:8551 barista:latest
+call docker run -p 8551:8551 -v /home/nagatoo/baristaData:/mnt/baristaData --add-host=database:172.17.0.1 --name barista barista:latest
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 ./gradlew build -x test
 docker rm barista
 docker build --tag=barista:latest .
-docker run -p 8551:8551 --add-host=database:172.17.0.1 --name barista barista:latest 
+docker run -p 8551:8551 -v /home/nagatoo/baristaData:/mnt/baristaData --add-host=database:172.17.0.1 --name barista barista:latest
